e235bcf9690873e95cf65ffd2648e737d11256be
Aaron Robson <ARobsonSE@hotmail.co.uk>
2018-04-01 23:34:02 +0100

    Null test call needs to be an empty Data.Map not list.

--- tests/Math/TSP/Tests.hs
+++ tests/Math/TSP/Tests.hs
@@ -3,12 +3,13 @@ module Math.TSP.Tests where
 import Test.Framework (defaultMain, testGroup, Test)
 import Test.Framework.Providers.HUnit
 import Test.HUnit hiding (Test)
+import Data.Map
 
 import Math.TSP
 
 tspSuite :: Test
 tspSuite = testGroup "TSP calls"
-    [ testCase "null case" (testTSP [] [] [])
+    [ testCase "null case" (testTSP [] (Data.Map.fromList []) [])
     ]
 
 testTSP :: Nodes -> WeightedEdges -> Route -> Assertion
